910a1c7b3baff6dc9d5cdd96597bebcc705fd883
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2022-09-21 17:21:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ export const config = {
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: false,
+  IS_SHOW_COLOR: true,
 
   // 功能开关,打开：true，关闭：false
   SWITCH: {
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "owQ0g5sacl3SfccPJuTfVxsquXl4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "xsGZrCIAtQluCLRO4XbsizLHntuVlqiRafgp9GMvyEs",
+      useTemplateId: "f3EDXirE_vZJnRDYdT2EIKXsWyjT01SyZO7gVaHK5V4",
       // 所在省份
       province: "江苏",
       // 所在城市
