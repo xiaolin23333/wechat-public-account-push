8686d24a2b5ad1e877130570f765fbeae9365a20
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2022-09-21 17:37:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "owQ0g5sacl3SfccPJuTfVxsquXl4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Ph2QvCTmFtF8Fx25kBCxFacoyBPsLYURmv8fws3MoqQ",
+      useTemplateId: "NRHLs_HNWvHocd-OUjCalaWFxOsMywPuKY3HPcBXaFg",
       // 所在省份
       province: "江苏",
       // 所在城市
