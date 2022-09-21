3330dfa8e71cd6854a46523b4f2ba8e2ce53f9ef
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2022-09-21 23:05:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "姚姚",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   //   id: "owQ0g5jAeR3AvgF2AQUQvu4N_o3o",
+      id: "owQ0g5jAeR3AvgF2AQUQvu4N_o3o",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Pbf7wJ83TO1o8UZ2NS-769-TAGy3O_CpvCXSUieP2IU",
       // 所在省份
