793694567adcb65ba7120617d908262d568624fc
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2022-10-18 22:32:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,11 +60,11 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "姚姚",
+      name: "",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "owQ0g5jAeR3AvgF2AQUQvu4N_o3o",
+      id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Pbf7wJ83TO1o8UZ2NS-769-TAGy3O_CpvCXSUieP2IU",
+      useTemplateId: "",
       // 所在省份
       province: "江苏",
       // 所在城市
@@ -78,7 +78,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "姚姚", "year": "2003", "date": "04-04"},
+        {"type": "*生日", "name": "", "year": "", "date": ""},
      //   {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
      //   {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
