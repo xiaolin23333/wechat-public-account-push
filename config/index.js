af3eedce82320ebea0e344bfb264e472e2f81a51
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2023-03-31 12:41:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,17 +60,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "安安",
+      name: "小音",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "owQ0g5sMAA01POojNRDJFMpn9jrA",
+      id: "owQ0g5sacl3SfccPJuTfVxsquXl4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "grwkA7zMAILH2x3oun1ALiTg8Cj9jcwsLTvJgNBC8lM",
+      useTemplateId: "Ea8-Auyclx5NZc5U605gSu7OoHBUEwKBYPIE6mLkwl0",
       // 所在省份
       province: "江苏",
       // 所在城市
-      city: "南京",
+      city: "无锡",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '08-15',
+      horoscopeDate: '04-11',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -78,7 +78,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "安安", "year": "2004", "date": "08-15"},
+        {"type": "*生日", "name": "小音", "year": "2001", "date": "04-11"},
      //   {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
      //   {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
@@ -93,7 +93,7 @@ export const config = {
     {
       name: "小林",
       id: "owQ0g5sacl3SfccPJuTfVxsquXl4",
-      useTemplateId: "grwkA7zMAILH2x3oun1ALiTg8Cj9jcwsLTvJgNBC8lM",
+      useTemplateId: "Ea8-Auyclx5NZc5U605gSu7OoHBUEwKBYPIE6mLkwl0",
       province: "江苏",
       city: "无锡",
       horoscopeDate: '04-11',
