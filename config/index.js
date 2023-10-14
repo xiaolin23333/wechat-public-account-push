13884c091a2cfc8e60885c18dd7c0ce9d149d5e0
xiaolin23333 <105870013+xiaolin23333@users.noreply.github.com>
2023-10-14 13:07:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "小妍",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "owQ0g5ta06ceTglUc27Amb5O2aHc",
+      id: "",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "opPRtWCKSbD4Ws7omvhaIlXL0aMFYK0k7c_pwcFu8-I",
       // 所在省份
@@ -92,7 +92,7 @@ export const config = {
     },
     {
       name: "小林",
-      id: "owQ0g5sacl3SfccPJuTfVxsquXl4",
+      id: "",
       useTemplateId: "02k5rOrpTPYLIMdG4ixmJttdra1UyzU8QE3R6Omad8Q",
       province: "江苏",
       city: "无锡",
